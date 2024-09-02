2babf93f5462cfb141fdce5490c536631c055a69
lancelot.bulte <lancelot.bulte@monlycee.net>
2024-09-02 18:28:45 +0200

    save before opti

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 #include <chrono>
 #include <vector>
 #include <algorithm>
-#include <locale>
-#include <codecvt>
 #include <conio.h>
 
 class Table
@@ -27,6 +25,7 @@ public:
     {
         this->table.push_front(std::deque<bool>(column, false));
         this->row++;
+        this->init_pos.first++;
     }
     void add_down()
     {
@@ -48,6 +47,7 @@ public:
             i.push_front(0);
         }
         this->column++;
+        this->init_pos.second++;
     }
 
     void pixel_in_life(size_t row_, size_t column_, std::deque<std::deque<bool>> &copy)
@@ -112,8 +112,27 @@ public:
         }
     }
 
-    // bool &operator()(int row, int column) { return table[row][column]; }
-    // const bool &operator()(int row, int column) const { return table[row][column]; }
+    void change_pos(int change, bool is_row = true)
+    {
+        if (is_row)
+        {
+            // Change row position
+            int new_pos = static_cast<int>(this->init_pos.first) + change;
+            if (new_pos >= 0 && new_pos + this->max_size.first < this->table.size())
+            {
+                this->init_pos.first = static_cast<size_t>(new_pos);
+            }
+        }
+        else
+        {
+            // Change column position
+            int new_pos = static_cast<int>(this->init_pos.second) + change;
+            if (new_pos >= 0 && new_pos + this->max_size.second < this->table[0].size())
+            {
+                this->init_pos.second = static_cast<size_t>(new_pos);
+            }
+        }
+    }
 
     void set_value(size_t row_, size_t column_)
     {
@@ -125,13 +144,12 @@ public:
 
     void print_table()
     {
-
-        const size_t size = (this->row) * (this->column + 1) + 1;
+        const size_t size = (this->max_size.first) * (this->max_size.second + 1) + 1;
         char buffer[size];
         size_t index = 0;
-        for (size_t i = 0; i < this->table.size(); i++)
+        for (size_t i = this->init_pos.first; i < this->max_size.first + this->init_pos.first; i++)
         {
-            for (size_t y = 0; y < this->table[i].size(); y++)
+            for (size_t y = this->init_pos.second; y < this->max_size.second + this->init_pos.second; y++)
             {
                 buffer[index] = (this->table[i][y] ? '#' : '.');
                 index++;
@@ -139,27 +157,67 @@ public:
             buffer[index] = '\n';
             index++;
         }
-        buffer[index] += '\n';
         fwrite(buffer, size, 1, stderr);
+        std::cout << this->column * this->row;
+    }
+
+    void loop(size_t number_iteration)
+    {
+        for (size_t i = 0; i < number_iteration; i++)
+        {
+            std::system("cls");
+            this->print_table();
+            this->update();
+            if (_kbhit())
+            {
+                int ch = _getch();
+                if (ch == 0 || ch == 0xE0)
+                {
+                    ch = _getch();
+                    switch (ch)
+                    {
+                    case 72:
+                        this->change_pos(-1);
+                        break;
+                    case 80:
+                        this->change_pos(1);
+                        break;
+                    case 75:
+                        this->change_pos(-1, false);
+                        break;
+                    case 77:
+                        this->change_pos(1, false);
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                else if (ch == 'q')
+                {
+                    break;
+                }
+            }
+        }
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    Table test(30, 30, std::make_pair(20, 20));
+    Table test(10000, 10000, std::make_pair(29, 49));
     test.set_value(15, 20);
     test.set_value(16, 20);
     test.set_value(17, 20);
     test.set_value(16, 19);
     test.set_value(17, 21);
-    for (size_t i = 0; i < 1000; i++)
-    {
-        std::system("cls");
-        test.print_table();
-        test.update();
-    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    test.update();
+
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << duration.count();
 
     return 0;
 }
-
-//! faire un truc pour se déplacer
